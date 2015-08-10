bd2e82f0d6d84e2c4acaeeb0a44a49b8f3a9ca6d
jsAaron <401545553@qq.com>
2015-08-10 17:22:39 +0800

    update

--- password-box/pwBox.js
+++ password-box/pwBox.js
@@ -1,77 +1,71 @@
 /**
  * 密码输入框
  */
-var pwBox = function (fn) {
-    //密码输入框
-    var keyboard = document.querySelector('.cm-modal-bd')
-    //密码显示框
-    var pwBox = document.querySelector('.cm-password-box');
-    //密码框的li框
-    var pwBox_li = pwBox.querySelectorAll("li");
+var pwBox = function(fn) {
 
-    //密码
-    var passwordArr = [];
+	var SUPPORT_TOUCH = ('ontouchstart' in window);
+	var START_EV = SUPPORT_TOUCH ? 'touchstart' : 'mousedown';
 
-    plat.execEvent('on', {
-        context: keyboard,
-        callback: {
-            start: start,
-            move: move,
-            end: end
+	//密码输入框
+	var keyboard = document.querySelector('.cm-modal-bd')
+	//密码显示框
+	var pwBox = document.querySelector('.cm-password-box');
+	//密码框的li框
+	var pwBox_li = pwBox.querySelectorAll("li");
+
+	//密码
+	var passwordArr = [];
+	 
+    function compatibilityEvent(e) {
+        var point;
+        if (e.touches && e.touches[0]) {
+            point = e.touches[0];
+        } else {
+            point = e;
         }
-    })
-
-    //增加密码
-    function addPassWord(value){
-        passwordArr.push(value)
-        pwBox_li[passwordArr.length - 1].children[0].style.display = "block"
+        return point
     }
 
-    //删除密码
-    function delPassWord(){
-        passwordArr.pop();
-        pwBox_li[passwordArr.length].children[0].style.display = "none"
-    }
+	keyboard.addEventListener(START_EV,function(e){
+		start(compatibilityEvent(e))
+	},false);
 
-    //检测数字
-    function isNumer(value) {
-        return /^[0-9]*$/.test(value)
-    }
+	function addPassWord(value) {
+		passwordArr.push(value)
+		pwBox_li[passwordArr.length - 1].children[0].style.display = "block"
+	}
 
-    function start(event) {
-        if (passwordArr.length > 5) return
-        var textContent = event.target.textContent
-        //如果是纯数字
-        if (isNumer(textContent)) {
-            textContent = Number(textContent)
-            addPassWord(textContent)
-            if(passwordArr.length === 6){
-                fn && fn(passwordArr)
-            }
-            return;
-        }
-        //删除
-        if(event.target.className === 'cm-keyboard-list'){
-            delPassWord()
-        }
-    }
+	function delPassWord() {
+		passwordArr.pop();
+		pwBox_li[passwordArr.length].children[0].style.display = "none"
+	}
 
-    function move(event) {
+	function isNumer(value) {
+		return /^[0-9]*$/.test(value)
+	}
 
-    }
+	function start(event) {
+		console.log(event)
+		if (passwordArr.length > 5) return
+		var textContent = event.target.textContent
+		if (isNumer(textContent)) {
+			textContent = Number(textContent)
+			addPassWord(textContent)
+			if (passwordArr.length === 6) {
+				fn && fn(passwordArr)
+			}
+			return;
+		}
+		if (event.target.className === 'cm-keyboard-list') {
+			delPassWord()
+		}
+	}
 
-    function end(event) {
+	return {
 
-    }
+		create: function() {
 
-
-    return {
-
-        create: function () {
-
-        }
-    }
+		}
+	}
 
 }
-
-
