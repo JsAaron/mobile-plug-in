3fe0bbc35d5ac6802e9eba645c7a1868eb927bc3
jsAaron <401545553@qq.com>
2015-08-16 13:51:47 +0800

    node

--- node/server.js
+++ node/server.js
@@ -3,6 +3,6 @@ http.createServer(function(req, res) {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+	res.end('Hello Worl1111222\n');
+}).listen(8888, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
